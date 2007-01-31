c83e1b237969d1f1b2d59c56cfa4f354fe36a2e3
michael <michael@elehack.net>
2007-01-31 15:38:40 -0600

    Initial checkin

---
+++ assert.c
@@ -0,0 +1,39 @@
+/*****************************************************************************
+ * assert.c - assertion framework for BogoTest
+ ****************************************************************************/
+
+#include <stdio.h>
+#include <stdargs.h>
+
+void _bt_assert(int expr, const char *file, int line, const char *msg, ...)
+{
+    va_list ap;
+
+    if (!expr) {
+        gchar *lead, *fmsg;
+        lead = g_strdup_printf("Failure at %s:%d", file, line);
+        va_start(ap, msg);
+        fmsg = g_strdup_vprintf(msg, ap);
+        va_end(ap);
+        fprintf(stderr, "%s: %s\n", lead, fmsg);
+        g_free(lead);
+        g_free(fmsg);
+        _bt_fail_test();
+    }
+}
+
+void _bt_assert_strings_equal(const char *act, const char *exp,
+        const char *file, int line, const char *expr)
+{
+    _bt_assert(g_string_equal(act, exp), file, line,
+            "string %s has incorrect value\n  Actual: '%s'\n  Expected: '%s'",
+            expr, act, exp);
+}
+
+void _bt_assert_doubles_equal(double act, double exp, double delta,
+        const char *file, int line, const char *expr)
+{
+    _bt_assert(fabs(act - exp) < delta, , file, line,
+            "double %f has incorrect value\n  Actual: %f\n  Expected: %f"
+            expr, act, exp);
+}
---
+++ bogotest.h
@@ -0,0 +1,61 @@
+/*****************************************************************************
+ * bogotest.h - master include file for BogoTest
+ *
+ * BogoTest (The Bogus Testing Framework for C) is a unit-testing framework
+ * for C programmers.
+ ****************************************************************************/
+
+#ifndef BOGOTEST_H
+#define BOGOTEST_H
+
+#ifndef BOGOTEST_DOUBLE_DELTA
+#define BOGOTEST_DOUBLE_DELTA 0.0001
+
+typedef void (*BTTestFunc)(gpointer);
+typedef gpointer (*BTTestSetupFunc)(void);
+typedef void (*BTTestTeardownFunc)(gpointer);
+
+typedef struct _BTTestInfo {
+    const char *name;
+    BTestFunc test;
+} BTTestInfo;
+#define BT_TEST_INFO_NULL { NULL, NULL }
+
+typedef struct _BTFixtureInfo {
+    BTestSetupFunc setup;
+    BTestTeardownFunc teardown;
+    BTestFunc *funcs;
+} BTFixtureInfo;
+#define BT_FIXTURE_INFO_NULL { NULL, NULL, NULL }
+
+typedef struct _BTSuiteInfo {
+    const char *name;
+    SBTFixtureInfo *fixtures;
+} BTSuiteInfo;
+#define BT_SUITE_INFO_NULL { NULL, NULL }
+
+int _bt_assert(int expr, const char *file, int line, const char *msg, ...);
+#define BT_ASSERT(expr) _bt_assert((expr), __FILE__, __LINE__, \
+        "assertion failed: `%s`", #expr)
+#define BT_ASSERT_MESSAGE(expr, msg, ...) _bt_assert((expr), __FILE__, __LINE__, msg, __VA_ARGS__)
+
+#define BT_ASSERT_PTR_NULL(ptr) _bt_assert((ptr) == NULL, __FILE__, __LINE__, \
+        "pointer %s isn't NULL", #ptr)
+#define BT_ASSERT_PTR_NULL_MESSAGE(ptr, msg, ...) _bt_assert(ptr == NULL, __FILE__, __LINE__, \
+        msg, __VA_ARGS__)
+#define BT_ASSERT_PTR_NOT_NULL(ptr) _bt_assert(ptr != NULL, __FILE__, __LINE__, \
+        "pointer %s is NULL", #ptr)
+#define BT_ASSERT_PTR_NOT_NULL_MESSAGE(ptr, msg, ...) _bt_assert(ptr != NULL, __FILE__, __LINE__, \
+        msg, __VA_ARGS__)
+
+void _bt_assert_strings_equal(const char *act, const char *exp,
+        const char *file, int line, const char *expr);
+#define BT_ASSERT_STRINGS_EQUAL(actual, expected) _bt_assert_strings_equal( \
+        (actual), (expected), __FILE__, __LINE__, #actual)
+
+void _bt_assert_doubles_equal(double act, double exp, double delta,(act, exp)(act, exp)
+        const char *file, int line, const char *expr);
+#define BT_ASSERT_DOUBLES_EQUAL(actual, expected) _bt_assert_doubles_equal( \
+        (actual), (expected), BOGOTEST_DOUBLE_DELTA, __FILE__, __LINE__, #actual)
+
+#define BOGOTEST_H
